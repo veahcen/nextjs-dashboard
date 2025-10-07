6b3657592d9c2b392680da74b48fd63db352b94e
mihai <liahim.veahcen@gmail.com>
2025-10-07 15:09:57 +0300

    fix: return in server functions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -35,9 +35,7 @@ export async function createInvoice(formData: FormData) {
     } catch (error) {
         // We'll also log the error to the console for now
         console.error(error);
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-        };
+        throw new Error("Database Error: Failed to Create Invoice.")
     }
 
     revalidatePath('/dashboard/invoices');
@@ -62,7 +60,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     } catch (error) {
         // We'll also log the error to the console for now
         console.error(error);
-        return { message: 'Database Error: Failed to Update Invoice.' };
+        throw new Error("Database Error: Failed to Update Invoice.")
     }
 
     revalidatePath('/dashboard/invoices');
