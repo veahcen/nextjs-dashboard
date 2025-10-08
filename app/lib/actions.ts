850c2704e02c6f3a5522e26bb467076cb8939f5e
mihai <liahim.veahcen@gmail.com>
2025-10-08 15:50:23 +0300

    feat: added authorization to the system with route settings, added metadata

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { AuthError } from 'next-auth';
+import {signIn} from "@/app/auth";
 
 const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
@@ -108,4 +110,23 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
